3c74f77cd9a2ea7939358b2c1a413bf1466a68ff
Olga Fedorova <FedorovaOlgaE@yandex.ru>
2022-01-12 22:52:16 +0300

    Sprint_4

---
+++ src/main/java/ru/yandex/praktikum/Account.java
@@ -0,0 +1,19 @@
+package ru.yandex.praktikum;
+
+public class Account
+{
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        return name.length() >= 3 &&
+                name.length() <= 19 &&
+                name.length() - name.replace(" ", "").length() == 1 &&
+                !name.startsWith(" ") &&
+                !name.endsWith(" ");
+
+    }
+}
